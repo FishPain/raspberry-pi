1c29f6db41d1be79d1f916b4809f969fc5d2ac1e
FishPain <itsfishpain@gmail.com>
2021-09-21 16:28:31 +0800

    take a photo every 1 min

--- camera.py
+++ camera.py
@@ -1,15 +1,16 @@
 import os
-from crontab import CronTab
+import time
+from datetime import date
 
 
 FRAME = 24
 
-#date-time = 0
 
-#os.system(f'fswebcam -F {FRAME} -r 1280x720 --no-banner image{date-time}.jpg')
+def take_photo(FRAME, date_time):
+    os.system(f'fswebcam -F {FRAME} -r 1280x720 --no-banner image{date_time}.jpg')
 
-
-def main():
-    with CronTab(user='root') as cron:
-        job = cron.new(command=f'fswebcam -F {FRAME} -r 1280x720 --no-banner image21.jpg')
-        job.minute.every(1)
+# a function that runs take_photo() every one hour
+def take_photos():
+    while True:
+        take_photo(FRAME, date.today().strftime('%Y%m%d%H%M%S'))
+        time.sleep(60)
