750f2794eb0a972512e94dfad7b5856b42d1a01c
FishPain <itsfishpain@gmail.com>
2021-09-21 16:30:16 +0800

    a

--- camera.py
+++ camera.py
@@ -7,7 +7,7 @@ FRAME = 24
 
 
 def take_photo(FRAME, date_time):
-    os.system(f'fswebcam -F {FRAME} -r 1280x720 --no-banner image{date_time}.jpg')
+    os.system(f"fswebcam -F {FRAME} -r 1280x720 --no-banner image{date_time}.jpg")
 
 # a function that runs take_photo() every one hour
 def take_photos():
