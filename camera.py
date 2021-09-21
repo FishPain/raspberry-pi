382de1ae438e8036e99be602b1cccc3ba9f61822
FishPain <itsfishpain@gmail.com>
2021-09-21 16:20:19 +0800

    a

--- camera.py
+++ camera.py
@@ -1,3 +1,15 @@
 import os
+from crontab import CronTab
 
-os.system('fswebcam -F 24 -r 1280x720 --no-banner image10.jpg')
+
+FRAME = 24
+
+#date-time = 0
+
+#os.system(f'fswebcam -F {FRAME} -r 1280x720 --no-banner image{date-time}.jpg')
+
+
+def main():
+    with CronTab(user='root') as cron:
+        job = cron.new(command=f'fswebcam -F {FRAME} -r 1280x720 --no-banner image21.jpg')
+        job.minute.every(1)
